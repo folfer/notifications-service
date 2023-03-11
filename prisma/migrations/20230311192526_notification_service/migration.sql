0bda62b90c6859f0c4286613fc5a594b35f6f525
Victor Calazans <victorcalazansramos@gmail.com>
2023-03-11 16:27:34 -0300

    feat: change for postgresql

--- prisma/migrations/20230311192526_notification_service/migration.sql
+++ prisma/migrations/20230311192526_notification_service/migration.sql
@@ -1,11 +1,14 @@
 -- CreateTable
 CREATE TABLE "Notification" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "recipientId" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "category" TEXT NOT NULL,
-    "readAt" DATETIME,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "readAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "canceledAt" TIMESTAMP(3),
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
